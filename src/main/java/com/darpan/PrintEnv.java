b421f64e3bfd36e81e9dc1d041f62d0293fa27a0
Darpan Lalwani <darpan.l@browserstack.com>
2024-08-30 14:55:00 +0530

    trying in a package

---
+++ src/main/java/com/darpan/PrintEnv.java
@@ -0,0 +1,8 @@
+package com.darpan;
+
+public class PrintEnv {
+    public static void main(String[] args) {
+        String dayOfWeek = System.getProperty("DAY_OF_WEEK");
+        System.out.println("DAY_OF_WEEK is: " + dayOfWeek);
+    }
+}
