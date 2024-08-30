bfd28f9306faf65290efdde25130f226a8c593b5
Darpan Lalwani <darpan.l@browserstack.com>
2024-08-30 15:26:11 +0530

    Final env variable working

--- src/main/java/com/darpan/PrintEnv.java
+++ src/main/java/com/darpan/PrintEnv.java
@@ -3,9 +3,11 @@ package com.darpan;
 public class PrintEnv {
     public static void main(String[] args) {
         String dayOfWeek = System.getProperty("DAY_OF_WEEK"); // Access system property
+        String source = "getProperty";
         if (dayOfWeek == null) {
             dayOfWeek = System.getenv("DAY_OF_WEEK"); // Fallback to environment variable
+            source = "getenv";
         }
-        System.out.println("DAY_OF_WEEK is: " + dayOfWeek);
+        System.out.println("Received env variable from " + source + " and DAY_OF_WEEK is: " + dayOfWeek);
     }
 }
