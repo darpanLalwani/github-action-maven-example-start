da6cdaefb175cd4100441f4f0052b141f4c298aa
Darpan Lalwani <darpan.l@browserstack.com>
2024-08-30 15:12:47 +0530

    updating way env variable is passed

--- src/main/java/com/darpan/PrintEnv.java
+++ src/main/java/com/darpan/PrintEnv.java
@@ -2,7 +2,10 @@ package com.darpan;
 
 public class PrintEnv {
     public static void main(String[] args) {
-        String dayOfWeek = System.getProperty("DAY_OF_WEEK");
+        String dayOfWeek = System.getProperty("DAY_OF_WEEK"); // Access system property
+        if (dayOfWeek == null) {
+            dayOfWeek = System.getenv("DAY_OF_WEEK"); // Fallback to environment variable
+        }
         System.out.println("DAY_OF_WEEK is: " + dayOfWeek);
     }
 }
