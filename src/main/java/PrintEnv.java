5f6e7e15426972ff10442d81eba3843581f7c159
darpanLalwani <110385131+darpanLalwani@users.noreply.github.com>
2024-08-30 14:25:09 +0530

    Create PrintEnv.java

---
+++ src/main/java/PrintEnv.java
@@ -0,0 +1,6 @@
+public class PrintEnv {
+    public static void main(String[] args) {
+        String dayOfWeek = System.getProperty("DAY_OF_WEEK");
+        System.out.println("DAY_OF_WEEK is: " + dayOfWeek);
+    }
+}
