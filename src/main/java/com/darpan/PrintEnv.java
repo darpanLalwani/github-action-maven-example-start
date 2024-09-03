2e4055332dd1fef8c05b0deae4e1b8eea13527e0
Darpan Lalwani <darpan.l@browserstack.com>
2024-09-03 17:05:33 +0530

    Passing actual param

--- src/main/java/com/darpan/PrintEnv.java
+++ src/main/java/com/darpan/PrintEnv.java
@@ -2,12 +2,12 @@ package com.darpan;
 
 public class PrintEnv {
     public static void main(String[] args) {
-        String dayOfWeek = System.getProperty("DAY_OF_WEEK"); // Access system property
+        String dayOfWeek = System.getProperty("IS_RERUN"); // Access system property
         String source = "getProperty";
         if (dayOfWeek == null) {
-            dayOfWeek = System.getenv("DAY_OF_WEEK"); // Fallback to environment variable
+            dayOfWeek = System.getenv("IS_RERUN"); // Fallback to environment variable
             source = "getenv";
         }
-        System.out.println("Received env variable from " + source + " and DAY_OF_WEEK is: " + dayOfWeek);
+        System.out.println("Received env variable from " + source + " and IS_RERUN is: " + dayOfWeek);
     }
 }
